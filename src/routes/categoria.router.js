26993391cff638e3ae554d5d4cb2c87dd5627849
Juan Manuel Goncalves De Faria <jmgoncalvesdefaria@fecea.edu.ar>
2026-04-21 21:23:44 -0300

    fix: Post Categorias y Get Categorias

--- src/routes/categoria.router.js
+++ src/routes/categoria.router.js
@@ -4,12 +4,23 @@ const productosRouter = require('./producto.router.js')
 
 const categoriaRouter = express.Router()
 
-categoriaRouter.post('/', (req, res) => {
+productosRouter.post('/:categoria', (req, res) => {
     const categoria = req.params.categoria
     const body = req.body
     productos.infoProductos[categoria].push(body)
     res.statusCode = 201
     res.end(JSON.stringify(body))
+})
+
+productosRouter.get('/:categoria', (req, res) => {
+    const categoria = req.params.categoria
+    const productosCategoria = productos.infoProductos[categoria]
+        if(productosCategoria){
+            res.end(JSON.stringify(productosCategoria))
+        } else {
+            res.statusCode = 404
+            res.end(`el Categoria ${categoria} no existe`)
+        }
 })
 
 module.exports = categoriaRouter
