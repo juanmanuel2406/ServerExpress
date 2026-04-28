1e6d3eea44e9e99e4ad0b1e1ab9da50d40c886c5
unknown <jmgoncalvesdefaria@fecea.edu.ar>
2026-04-28 20:55:44 -0300

    feat: armo la arquitectura general del serivcio

--- src/routes/categoria.router.js
+++ src/routes/categoria.router.js
@@ -1,26 +1,10 @@
+//Rutas de Categoria
+const { getCategoria, postCategoria} = require('../controllers/categoria.controller.js')
 const express = require('express')
-const productos = require('../data/productos')
-const productosRouter = require('./producto.router.js')
 
 const categoriaRouter = express.Router()
 
-productosRouter.post('/:categoria', (req, res) => {
-    const categoria = req.params.categoria
-    const body = req.body
-    productos.infoProductos[categoria].push(body)
-    res.statusCode = 201
-    res.end(JSON.stringify(body))
-})
-
-productosRouter.get('/:categoria', (req, res) => {
-    const categoria = req.params.categoria
-    const productosCategoria = productos.infoProductos[categoria]
-        if(productosCategoria){
-            res.end(JSON.stringify(productosCategoria))
-        } else {
-            res.statusCode = 404
-            res.end(`el Categoria ${categoria} no existe`)
-        }
-})
+categoriaRouter.post('/', postCategoria)
+categoriaRouter.get('/', getCategoria)
 
 module.exports = categoriaRouter
--- src/routes/producto.router.js
+++ src/routes/producto.router.js
@@ -1,29 +1,17 @@
 const express = require('express')
-const productos = require('../data/productos')
+const { 
+    getProducto,
+    getProductoPorCategoria,
+    postProducto
+} = require('../controllers/producto.controller')
 
 const productosRouter = express.Router()
+exports.productosRouter = productosRouter
 
-productosRouter.get('/productos', (req, res) =>{
-    res.end(JSON.stringify(productos.infoProductos))
-})
+productosRouter.get('/', getProducto)
 
-productosRouter.get('/:categoria', (req, res) => {
-    const categoria = req.params.categoria
-    const productosCategoria = productos.infoProductos[categoria]
-        if(productosCategoria){
-            res.end(JSON.stringify(productosCategoria))
-        } else {
-            res.statusCode = 404
-            res.end(`el Categoria ${categoria} no existe`)
-        }
-})
+productosRouter.get('/:categoria', getProductoPorCategoria)
 
-productosRouter.post('/:categoria', (req, res) => {
-    const categoria = req.params.categoria
-    const body = req.body
-    productos.infoProductos[categoria].push(body)
-    res.statusCode = 201
-    res.end(JSON.stringify(body))
-})
+productosRouter.post('/:categoria', postProducto)
 
 module.exports = productosRouter
