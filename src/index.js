9f3cda8975c5f183e799024c60814c9b44b33961
Esteban Faría <estebanfaria20@gmail.com>
2026-06-07 15:28:02 -0300

    feat: agrego modulo autenticacion usuarios (login/registro) con JWT y bcrypt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const productoRouter = require('./routes/producto.router')
 const categoriaRouter = require('./routes/categoria.router')
+const usuarioRouter = require('./routes/usuario.router')
 const { errorLog, errorHandler } = require('./middlewares/errorHandler')
 const { testConnection } = require('./db/connection')
 require('dotenv').config()
@@ -10,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use('/productos', productoRouter)
 app.use('/categorias', categoriaRouter)
+app.use('/usuarios', usuarioRouter)
 
 app.get('/', (req, res) => {
     res.end('Servidor desarrollado con express')
